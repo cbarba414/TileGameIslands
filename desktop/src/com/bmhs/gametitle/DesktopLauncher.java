854471274cd267f33368ae8414019914ae91e19f
cbarba414 <cristinabarba846@gmail.com>
2024-01-24 11:50:51 -0800

    first ever commit ever

--- desktop/src/com/bmhs/gametitle/DesktopLauncher.java
+++ desktop/src/com/bmhs/gametitle/DesktopLauncher.java
@@ -3,7 +3,7 @@ package com.bmhs.gametitle;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.bmhs.gametitle.game.utils.GameHandler;
-
+/hey
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
